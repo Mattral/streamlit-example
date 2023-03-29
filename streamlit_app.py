3004b5bb68641270861dfd6ce75450b7aac7652e
Mattral <88831350+Mattral@users.noreply.github.com>
2023-03-29 15:45:10 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,139 +1,35 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
-import openai 
-from pages.datetime import datetime
-from streamlit.components.v1 import html
-import pandas as pd 
-import csv 
-st.set_page_config(page_title="Envisage with me")
-
-
-html_temp = """
-                <div style="background-color:{};padding:1px">
-                
-                </div>
-                """
-
-button = """
-<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="nainiayoub" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+import openai
+"""
+# Welcome to Streamlit!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
-with st.sidebar:
-    st.markdown("""
-    # About 
-    A helper tool built on GPT-3 to generate ideas on a given topic. 
-    """)
-    st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
-    st.markdown("""
-    # How does it work
-    Simply enter the topic of interest in the text field below and ideas will be generated.
-    You can also download the output as txt.
-    """)
-    st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
-    
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-input_text = None
-if 'output' not in st.session_state:
-    st.session_state['output'] = 0
+    points_per_turn = total_points / num_turns
 
-if st.session_state['output'] <=2:
-    st.markdown("""
-    # Brainstorming Buddy
-    """)
-    input_text = st.text_input("Brainstorm ideas for", disabled=False, placeholder="What's on your mind?")
-    st.session_state['output'] = st.session_state['output'] + 1
-else:
-    # input_text = st.text_input("Brainstorm ideas for", disabled=True)
-    st.info("Thank you! Refresh for more brainstorming💡")
-    st.markdown('''
-    <a target="_blank" style="color: black" href="https://twitter.com/intent/tweet?text=I%20just%20used%20the%20Brainstorming%20Buddy%20streamlit%20helper%20tool%20by%20@nainia_ayoub!%0A%0Ahttps://brainstorming-buddy.streamlit.app/">
-        <button class="btn">
-            Tweet about this!
-        </button>
-    </a>
-    <style>
-    .btn{
-        display: inline-flex;
-        -moz-box-align: center;
-        align-items: center;
-        -moz-box-pack: center;
-        justify-content: center;
-        font-weight: 400;
-        padding: 0.25rem 0.75rem;
-        border-radius: 0.25rem;
-        margin: 0px;
-        line-height: 1.6;
-        color: #fff;
-        background-color: #00acee;
-        width: auto;
-        user-select: none;
-        border: 1px solid #00acee;
-        }
-    .btn:hover{
-        color: #00acee;
-        background-color: #fff;
-    }
-    </style>
-    ''',
-    unsafe_allow_html=True
-    )
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-hide="""
-<style>
-footer{
-	visibility: hidden;
-    position: relative;
-}
-.viewerBadge_container__1QSob{
-    visibility: hidden;
-}
-#MainMenu{
-	visibility: hidden;
-}
-<style>
-"""
-st.markdown(hide, unsafe_allow_html=True)
-
-html(button, height=70, width=220)
-st.markdown(
-    """
-    <style>
-        iframe[width="220"] {
-            position: fixed;
-            bottom: 60px;
-            right: 40px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-if input_text:
-    prompt = "Brainstorm ideas for "+str(input_text)
-    if prompt:
-        openai.api_key = st.secrets["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"]
-        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=150)
-        brainstorming_output = response['choices'][0]['text']
-        today = datetime.today().strftime('%Y-%m-%d')
-        topic = "Brainstorming ideas for: "+input_text+"\n@Date: "+str(today)+"\n"+brainstorming_output
-        
-        st.info(brainstorming_output)
-        filename = "brainstorming_"+str(today)+".txt"
-        btn = st.download_button(
-            label="Download txt",
-            data=topic,
-            file_name=filename
-        )
-        fields = [input_text, brainstorming_output, str(today)]
-        # read local csv file
-        r = pd.read_csv('./data/prompts.csv')
-        if len(fields)!=0:
-            with open('./data/prompts.csv', 'a', encoding='utf-8', newline='') as f:
-                # write to csv file (append mode)
-                writer = csv.writer(f, delimiter=',', lineterminator='\n')
-                writer.writerow(fields)
-
-        
-        
-
-        
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
