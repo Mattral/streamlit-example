0f873e215e590b5dbe1a4ae210989ca7d23daad9
Mattral <88831350+Mattral@users.noreply.github.com>
2023-03-29 15:46:27 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import openai
+import openai as ai
 """
 # Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
